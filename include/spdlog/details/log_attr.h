4433c53526ab38665c3eaf29304fb36aef825f92
Bailey Chittle <baileyc@tessonics.com>
2023-03-10 11:42:16 -0500

    using structs to define type conversions

--- include/spdlog/details/log_attr.h
+++ include/spdlog/details/log_attr.h
@@ -11,6 +11,52 @@ namespace details {
 // template<typename T>
 // concept composable = std::same_as<T, bool> || std::integral<T> || std::floating_point<T> || std::convertible_to<T, std::string_view>;
 
+
+struct Key 
+{
+    std::string _key;
+
+    Key(string_view_t k) {
+        scramble(_key, k);
+    }
+    Key(std::string&& k) {
+        scramble(_key, k);
+    }
+    Key(const char* k) {
+        scramble(_key, k);
+    }
+};
+
+struct Value
+{
+    std::string _value;
+
+    Value(string_view_t v) {
+        scramble(_value, v);
+    }
+    Value(std::string&& v) {
+        scramble(_value, v);
+    }
+    Value(const char* v) {
+        scramble(_value, v);
+    }
+    Value(long v) {
+        _value = std::to_string(v);
+    }
+    Value(long long v) {
+        _value = std::to_string(v);
+    }
+    Value(unsigned long v) {
+        _value = std::to_string(v);
+    }
+    Value(unsigned long long v) {
+        _value = std::to_string(v);
+    }
+    Value(bool v) {
+        _value = v ? "true" : "false";
+    }
+};
+
 struct attr
 {
     std::string key;
@@ -110,6 +156,8 @@ public:
     attr(std::string&& k, bool v)
         : attr{string_view_t{k}, v}
     {}
+    attr(Key&& k, Value&& v) : key(std::move(k._key)), value(std::move(v._value)) {}
+    attr(Key const& k, Value const& v) : key(k._key), value(v._value) {}
 };
 
 } // namespace details
