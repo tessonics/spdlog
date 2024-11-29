3fc14822efc6f316d804265b34582eb4c55d4628
gabime <gmelman1@gmail.com>
2024-11-29 12:20:21 +0200

    Fix source_loc test

--- tests/test_source_location.cpp
+++ tests/test_source_location.cpp
@@ -1,20 +1,21 @@
-#define SPDLOG_SOURCE_LOCATION
+#ifndef SPDLOG_NO_SOURCE_LOC
+
 #include "includes.h"
 #include "spdlog/sinks/ostream_sink.h"
 #include "test_sink.h"
 
-#if defined(SPDLOG_HAVE_STD_SOURCE_LOCATION) || defined(SPDLOG_HAVE_EXPERIMENTAL_SOURCE_LOCATION)
 
 using spdlog::details::os::default_eol;
 
 TEST_CASE("test_source_location", "[source_location]") {
     std::ostringstream oss;
     auto oss_sink = std::make_shared<spdlog::sinks::ostream_sink_st>(oss);
-    spdlog::logger oss_logger("oss", oss_sink);
-    oss_logger.set_pattern("%s:%# %v");
+    auto oss_logger = std::make_shared<spdlog::logger>("oss", oss_sink);
+    //spdlog::logger oss_logger("oss", oss_sink);
+    oss_logger->set_pattern("%s:%# %v");
 
-    oss_logger.info("Hello {}", "source location");
-    REQUIRE(oss.str() == std::string("test_source_location.cpp:16 Hello source location") + default_eol);
+    SPDLOG_LOGGER_CALL(oss_logger, spdlog::level::info, "Hello {}", "source location");
+    REQUIRE(oss.str() == std::string("test_source_location.cpp:17 Hello source location") + default_eol);
 }
 
 #endif
