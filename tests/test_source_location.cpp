c7fc302710ba24e9439fdddaa661de4a9662f59b
gabime <gmelman1@gmail.com>
2024-11-29 13:00:13 +0200

    Fixed soruce_loc test

--- tests/test_source_location.cpp
+++ tests/test_source_location.cpp
@@ -5,7 +5,6 @@
 #include "includes.h"
 #include "test_sink.h"
 
-
 // test with source location
 TEST_CASE("test_source_location", "[source_location]") {
     auto test_sink = std::make_shared<spdlog::sinks::test_sink_mt>();
@@ -14,11 +13,11 @@ TEST_CASE("test_source_location", "[source_location]") {
     // test with source location with parameters
     SPDLOG_LOGGER_CALL(logger, spdlog::level::info, "Hello {}", "source location");
     REQUIRE(test_sink->lines().size() == 1);
-    REQUIRE(test_sink->lines()[0] == "test_source_location.cpp:15 Hello source location");
+    REQUIRE(test_sink->lines()[0] == "test_source_location.cpp:14 Hello source location");
     // test with source location without parameters
     SPDLOG_LOGGER_CALL(logger, spdlog::level::info, "Hello");
     REQUIRE(test_sink->lines().size() == 2);
-    REQUIRE(test_sink->lines()[1] == "test_source_location.cpp:19 Hello");
+    REQUIRE(test_sink->lines()[1] == "test_source_location.cpp:18 Hello");
 }
 
 
